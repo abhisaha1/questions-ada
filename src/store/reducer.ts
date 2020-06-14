47aab772f6548c11a0a626f4aaf0f98a5c529aba
Abhishek Saha <abhisheksaha11@gmail.com>
2020-06-14 14:34:34 +0200

    basic reducers

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -7,6 +7,42 @@ export const reducer: React.Reducer<IGlobalState, IAction> = (
   action,
 ): IGlobalState => {
   switch (action.type) {
+    case ActionType.SET_QUESTIONS: {
+      return { ...state, questions: action.payload, cursor: 0 };
+    }
+
+    case ActionType.SELECT_ANSWER: {
+      const { cursor, value } = action.payload;
+      return {
+        ...state,
+        answers: {
+          ...state.answers,
+          [cursor]: value,
+        },
+      };
+    }
+
+    case ActionType.SET_ANSWERS: {
+      return {
+        ...state,
+        answers: { ...action.payload },
+      };
+    }
+
+    case ActionType.SET_QUESTION_CURSOR: {
+      return {
+        ...state,
+        cursor: action.payload,
+      };
+    }
+
+    case ActionType.SET_RESULT: {
+      return {
+        ...state,
+        result: action.payload,
+      };
+    }
+
     default:
       return state;
   }
