b7fc85ecce82d08fdf397c7c131b77a401a347cc
Abhishek Saha <abhisheksaha11@gmail.com>
2020-06-14 14:30:52 +0200

    initial boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+// import { StoreProvider } from "./store";
+
+ReactDOM.render(
+  // <StoreProvider>
+  <App />,
+  // </StoreProvider>,
+  document.getElementById("root"),
+);
